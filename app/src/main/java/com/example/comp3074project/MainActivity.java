44ef39ea2c6397079f42c352afae5960a90cdac5
enezihe <enezihe@gmail.com>
2025-11-26 20:01:08 -0500

    Navigation + layout fix

---
+++ app/src/main/java/com/example/comp3074project/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.comp3074_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.nav_view);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
