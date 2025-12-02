39ac46dd5d34334d73e2739e76300a911f0915f0
James Jones <jjsfitnesstoronto@gmail.com>
2025-12-02 04:09:05 -0500

    Fixed remainining issues with Reminders functionality

--- app/src/main/java/com/example/comp3074project/ui/reminders/RemindersFragment.java
+++ app/src/main/java/com/example/comp3074project/ui/reminders/RemindersFragment.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -39,18 +41,27 @@ public class RemindersFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // Set up RecyclerView
         RecyclerView recyclerView = view.findViewById(R.id.recyclerViewReminders);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
         adapter = new ReminderAdapter();
         recyclerView.setAdapter(adapter);
 
+        // Active reminders count
         tvActiveReminders = view.findViewById(R.id.tvActiveReminders);
 
+        // Initialize ViewModel
         reminderViewModel = new ViewModelProvider(this).get(ReminderViewModel.class);
 
         // Observe LiveData from ViewModel
         reminderViewModel.getAllReminders().observe(getViewLifecycleOwner(), this::updateUI);
+
+        // Add New Reminder button listener
+        Button btnAddNewReminder = view.findViewById(R.id.btn_add_new_reminder);
+        btnAddNewReminder.setOnClickListener(v ->
+                NavHostFragment.findNavController(this)
+                        .navigate(R.id.action_navigation_reminders_to_addReminderFragment)
+        );
     }
 
     private void updateUI(List<ReminderEntity> reminders) {
